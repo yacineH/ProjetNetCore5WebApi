83ecdf88c569c54fc5e10a96e5eec69ce64d6537
Yacine <yacine.hayat@gmail.com>
2021-10-11 23:28:04 +0200

    Persisting data into container with image MongoDb => create a new repository for MongoDb

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Catalog.Repositories;
+using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +13,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 namespace Catalog
 {
@@ -26,12 +31,25 @@ namespace Catalog
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
-            //to register the dependency in the service provider 
-            //singleton = one copy of the instance of a type across entire life time of our service  
-            services.AddSingleton<IItemsRepository,InMemItemsRepository>();
+        {   
+            //To indicate how to serialize the date and the Guid to MongoDb
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
             
+            //Configure MongoDbClient
+            services.AddSingleton<IMongoClient>(ServiceProvider => 
+            {
+              var settings=Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();  
+              return new MongoClient(settings.ConnectionString);
+            });
+
+            //1-to register the dependency in the service provider 
+            //singleton = one copy of the instance of a type across entire life time of our service  
+            //services.AddSingleton<IItemsRepository,InMemItemsRepository>();
+            
+            //2-to switch to MongoRepository
+             services.AddSingleton<IItemsRepository,MongoDbItemsRepository>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
