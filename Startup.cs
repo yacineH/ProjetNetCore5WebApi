1f88c48b2ab428656885f685bf0e0981a0c1d940
Yacine <yacine.hayat@gmail.com>
2021-10-12 12:36:50 +0200

    Health check with format content in json for both {ready and live}

--- Startup.cs
+++ Startup.cs
@@ -91,10 +91,29 @@ namespace Catalog
                 endpoints.MapControllers();
                 
                 endpoints.MapHealthChecks("/health/ready",new HealthCheckOptions{
-                    Predicate = (check) => check.Tags.Contains("ready")
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter=async(context,report)=>
+                    {
+                        var result=JsonSerializer.serialize(
+                            
+                                new{
+                                    status=report.Status.ToString();
+                                    checks=report.Entries.Select(entry=>new {
+                                        name = entry.key,
+                                        status = entry.Value.Status.ToString(),
+                                        exception = entry.Value.Exception!=null entry.Value.Exception.Message:"none",
+                                        duration = entry.Value.Duration.ToString()
+                                    })
+                                }                            
+                        );
+                    }
                 });
                 
-                  endpoints.MapHealthChecks("/health/live",new HealthCheckOptions{
+                //format the content in json 
+                context.Response.ContentType = MediaTypesNames.Application.Json;
+                await context.Response.WriteAsync(result)
+
+                endpoints.MapHealthChecks("/health/live",new HealthCheckOptions{
                     Predicate = (_) => false
                 });
 
