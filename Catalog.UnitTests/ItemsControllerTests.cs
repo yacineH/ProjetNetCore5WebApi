39dd4254372dc2cfd15d3ed70b833b06511b794a
Yacine <yacine.hayat@gmail.com>
2021-10-13 13:15:05 +0200

    Add projectTest with class that test the conttroller ItemsController with naming convention

---
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace Catalog.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetItemAsync_WithUnexistingItem_ReturnNotFound()
+        {
+            //Arrange
+             
+            //Act
+
+            //Assert
+
+        }
+    }
+}
